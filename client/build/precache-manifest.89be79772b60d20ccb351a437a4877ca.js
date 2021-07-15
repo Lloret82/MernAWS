5f6ae48d383ae92c4bd2e6710fddb3b272cac9a1
lloret82 <adbmanchester@gmail.com>
2021-07-15 12:40:00 +0100

    test

---
+++ client/build/precache-manifest.89be79772b60d20ccb351a437a4877ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb16b87ec66a045eb4e2cce6f14470d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aea33edb3e19655018c8",
+    "url": "/static/css/main.7226347f.chunk.css"
+  },
+  {
+    "revision": "bafaf35bee668ec27e2d",
+    "url": "/static/js/2.0aa8c592.chunk.js"
+  },
+  {
+    "revision": "aea33edb3e19655018c8",
+    "url": "/static/js/main.9f95d125.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
